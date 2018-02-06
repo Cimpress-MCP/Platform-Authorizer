bf804fe233c8d7f3c0e36f542c3b6d779497ba77
Christopher Osborn <cosborn@cimpress.com>
2018-02-06 17:44:52 -0500

    Collect Metrics on Authorizer Use
    JSON log messages are `console.log`'d to CloudWatch, to which a lambda
    is subscribed. This second lambda writes to CloudWatch metrics.
    
    - ⒨ Tighten Static Analysis
    - ⒨ Tighten IAM Roles
    
    [I promise]

--- authorizer.js
+++ authorizer.js
@@ -1,17 +1,17 @@
 /**
  * An authorization lambda function for the Cimpress Mass Customization Platform.
- * @copyright 2017 Cimpress, Inc.
+ * @copyright 2018 Cimpress, Inc.
  */
 
-'use strict'
-
-import { promisifyAll } from 'bluebird'
+import { promisify, promisifyAll } from 'bluebird'
+import { decode, verify } from 'jsonwebtoken'
 import JwksClient from 'jwks-rsa'
 import { resolve } from 'url'
 
-const MISCONFIGURATION = "This authorizer is not configured as a 'TOKEN' authorizer."
-const UNAUTHORIZED = new Error('Unauthorized')
+const AUDIENCE = 'https://api.cimpress.io/'
 const AUTHORITY = 'https://cimpress.auth0.com/'
+const MISCONFIGURATION = new Error("This authorizer is not configured as a 'TOKEN' authorizer.")
+const UNAUTHORIZED = new Error('Unauthorized')
 
 const jwksUri = resolve(AUTHORITY, '/.well-known/jwks.json')
 const client = promisifyAll(new JwksClient({
@@ -19,13 +19,13 @@ const client = promisifyAll(new JwksClient({
   rateLimit: true,
   jwksUri
 }))
-const { decode, verifyAsync } = promisifyAll(require('jsonwebtoken'))
+const verifyAsync = promisify(verify)
 
 /**
  * A callback indicating success or failure to the calling entity.
  *
  * @callback eventCallback
- * @param {string|Error} failure - A description of the failure.
+ * @param {String|Error} failure - A description of the failure.
  * @param {Object} success - An API Gateway authorizer response document.
  * @returns {void}
  */
@@ -34,22 +34,31 @@ const { decode, verifyAsync } = promisifyAll(require('jsonwebtoken'))
  * An event indicating a request coming into API Gateway that requires authorization.
  *
  * @typedef {Object} ApiGatewayAuthorizationEvent
- * @property {!string} type - The type of authorization that has been requested.
- * @property {!string} authorizationToken - The token to be checked for authentication.
- * @property {!string} methodArn - The ARN of the method which requires authorization.
+ * @property {!String} type - The type of authorization that has been requested.
+ * @property {!String} authorizationToken - The token to be checked for authentication.
+ * @property {!String} methodArn - The ARN of the method which requires authorization.
  */
 
 /**
  * Performs JWT Bearer authentication, and calls the provided callback.
  *
  * @async
- * @function handler
+ * @function default
  * @param {!ApiGatewayAuthorizationEvent} event - The event that caused this function to be invoked.
  * @param {!Object} context - The context associated with the event.
  * @param {!eventCallback} callback - A function that will be called when authentication succeeds or fails.
  * @returns {!Promise.<void>} - A promise which, when resolved, signals the result of authorization.
  */
-export async function handler ({ type: eventType, authorizationToken: token }, context, callback) {
+export default async function ({ type: eventType, authorizationToken: token, methodArn: arn }, context, callback) {
+  // note(cosborn) We're still gonna collect metrics if someone wastes our time.
+  const [ , , , , accountId, apiGatewayInfo ] = arn.split(':') // note(cosborn) arn:aws:execute-api:<regionId>:<accountId>:<apiId>/<stage>/<method>/<resourcePath>
+  const [ apiId ] = apiGatewayInfo.split('/')
+  console.log(JSON.stringify({
+    namespace: 'Platform Authorizer',
+    accountId,
+    apiId
+  }))
+
   if (eventType !== 'TOKEN') { // note(cosborn) Configuration check.
     return callback(MISCONFIGURATION)
   }
@@ -75,7 +84,7 @@ export async function handler ({ type: eventType, authorizationToken: token }, c
     const key = await client.getSigningKeyAsync(kid)
       .then(({ publicKey, rsaPublicKey }) => publicKey || rsaPublicKey)
     const { sub, scope } = await verifyAsync(tokenValue, key, {
-      audience: 'https://api.cimpress.io/',
+      audience: AUDIENCE,
       issuer: AUTHORITY
     })
     const resp = {
---
+++ reporter.js
@@ -0,0 +1,77 @@
+/**
+ * An metrics reporter lambda function for the Platform Authorizer.
+ * @copyright 2018 Cimpress, Inc.
+ */
+
+import { CloudWatch } from 'aws-sdk'
+import { promisify } from 'bluebird'
+import { Buffer } from 'buffer'
+import { gunzip } from 'zlib'
+
+const client = new CloudWatch()
+const gunzipAsync = promisify(gunzip)
+
+const metric = {
+  MetricName: 'UseCount',
+  Value: 1
+}
+
+/**
+ * A container for CloudWatch log data.
+ *
+ * @typedef {Object} AwsLogs
+ * @property {!String} data - The contents of the log message, gzipped and Base64 encoded.
+ */
+
+/**
+ * An event indicating a log message being written to CloudWatch that optionally matches the supplied filter.
+ *
+ * @typedef {Object} CloudWatchLogEvent
+ * @property {!AwsLogs} awslogs - A container for the log data.
+ */
+
+/**
+ * Reports metrics as written to CloudWatch.
+ *
+ * @async
+ * @function default
+ * @param {!CloudWatchLogEvent} event - The event that caused this function to be invoked.
+ * @returns {!Promise.<void>} - A promise which, when resolved, signals the result of authorization.
+ */
+export default async function ({ awslogs: { data: input } }) {
+  const { logEvents } = await gunzipAsync(Buffer.from(input, 'base64'))
+    .then(r => r.toString('utf8'))
+    .then(JSON.parse)
+
+  const metricData = logEvents
+    .map(({ message }) => message.split('\t'))
+    .map(([ , , event ]) => JSON.parse(event))
+    .map(({ accountId, apiId }) => [
+      {
+        Dimensions: [
+          {
+            Name: 'Account',
+            Value: accountId
+          }
+        ]
+      },
+      {
+        Dimensions: [
+          {
+            Name: 'Account',
+            Value: accountId
+          },
+          {
+            Name: 'API',
+            Value: apiId
+          }
+        ]
+      }
+    ]).reduce((a, b) => a.concat(b), []) // flatMap
+    .map(ds => Object.assign({}, metric, ds))
+
+  return client.putMetricData({
+    MetricData: metricData,
+    Namespace: 'Platform Authorizer'
+  }).promise()
+}
