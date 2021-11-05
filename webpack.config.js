ea0678988cfe5aab9553281d516b0217978d6d5a
Chris/0 <cosborn@cimpress.com>
2021-11-05 15:53:20 -0400

    Use AWS-Provided JWT Verification Library
    - Update Build Spec
    - Update Build Dependencies
    - Get Strict about Linting

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,22 @@
  * @license Apache-2.0
  */
 
-'use strict'
-
 module.exports = {
-  entry: { authorizer: './authorizer.js' },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  output: {
-    library: {
-      type: 'commonjs2'
-    }
-  },
-  stats: 'errors-only',
-  target: 'node'
-}
+    "entry": {"authorizer": "./authorizer.js"},
+    "mode": "development",
+    "module": {
+        "rules": [
+            {
+                "exclude": /node_modules/u,
+                "loader": "babel-loader",
+                "test": /\.js$/u
+            }
+        ]
+    },
+    "output": {
+        "library": {
+            "type": "commonjs2"
+        }
+    },
+    "target": "node"
+};
